4e603fd11c4a2d95eb7a83139f27b63f59d4dc39
KapilKumar2002 <kapil012002@gmail.com>
2024-05-12 19:34:34 +0530

    some parts added

--- src/components/discussions/Discussions.jsx
+++ src/components/discussions/Discussions.jsx
@@ -65,7 +65,7 @@ const Discussions = () => {
         {list.length >= 4 && (
           <div
             onClick={() => setShowAll(!showAll)}
-            className="cursor-pointer px-2 bg-white py-1 border text-violet-500 justify-center border-violet-400 md:w-32 w-24 rounded-lg absolute left-[50%] -translate-y-[50%] -translate-x-[50%] flex gap-2 items-center"
+            className="cursor-pointer px-2 bg-white py-1 border text-violet-500 justify-center border-violet-400 md:w-32 w-28 rounded-lg absolute left-[50%] -translate-y-[50%] -translate-x-[50%] flex gap-2 items-center"
           >
             <span className="font-semibold md:text-sm text-xs">
               {showAll ? "Show less" : "Show more"}
